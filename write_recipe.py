c738a2a3ab97647bf362ca3c3f27cfbea7679d49
Sam Griffith <garfunkelnator@gmail.com>
2017-07-20 17:46:53 -0400

    Create write_recipe.py

---
+++ write_recipe.py
@@ -0,0 +1,13 @@
+
+
+class RecipeTable:
+  def Recipe():
+    def create_recipe_dictionary(n):
+      recipe = {}
+      for r in recipe:
+        r = {user input("Item #: "):user input("Amount: ")
+        recipe.append(r)
+        return recipe
+      print recipe
+        
+  
