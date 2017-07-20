4ac02166d66fdc44eef4bf1e2757d3da406219df
Sam Griffith <garfunkelnator@gmail.com>
2017-07-20 13:19:47 -0400

    Create v_1.py

---
+++ v_1.py
@@ -0,0 +1,18 @@
+''' V_1 seeks to effectively and effeciently prepare 100 individual recipes more effeciently than an average house /wife/husband/non-gender specific person/ '''
+
+import recipes
+import time
+import reusables
+import mypyQR
+import box
+
+
+
+def read_recipe():
+  qr_read()
+  return code
+  
+  
+def supplies_check():
+  
+  
